fdd224bc08975061d442e9bf3c7b23ebb9dc2192
Munsen <munsenab@quine.cs.grinnell.edu>
2020-02-12 14:26:10 -0600

    HW4: Dynamic philosopher generation

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,5 +1,10 @@
 class HomepageController < ApplicationController
   def home
+    philosophers = Array["Pierre Bayle", "Robert Boyle", "Johannes Clauberg",
+                         "Ralph Cudworth", "Pieter De la Court", "Marin Mersenne",
+                         "Pierre Gassendi", "Lodewijk Meyer", "Gisbertus Voetius",
+                         "Franciscus Van den Enden", "Mary Wollstonecraft"]
+    @random_philosopher = philosophers.shuffle.first
   end
 
   def about
