5953a5a0a571f3a6f1926510ff6d01581dcc5fc4
Munsen <munsenab@quine.cs.grinnell.edu>
2020-02-25 15:08:27 -0600

    HW6: adding tests

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,10 +1,10 @@
 class HomepageController < ApplicationController
   def home
-    philosophers = Array["Pierre Bayle", "Robert Boyle", "Johannes Clauberg",
+    @philosophers = Array["Pierre Bayle", "Robert Boyle", "Johannes Clauberg",
                          "Ralph Cudworth", "Pieter De la Court", "Marin Mersenne",
                          "Pierre Gassendi", "Lodewijk Meyer", "Gisbertus Voetius",
                          "Franciscus Van den Enden", "Mary Wollstonecraft"]
-    @random_philosopher = philosophers.shuffle.first
+    @random_philosopher = @philosophers.shuffle.first
   end
 
   def about
--- test/controllers/homepage_controller_test.rb
+++ test/controllers/homepage_controller_test.rb
@@ -4,7 +4,7 @@ class HomepageControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = "Lesser-Known Enlightenment Philosophers"
   end
-  
+
   test "should get home" do
     get '/'
     assert_response :success
@@ -21,5 +21,20 @@ class HomepageControllerTest < ActionDispatch::IntegrationTest
     get '/about'
     assert_response :success
     assert_select "title", "About | #{@base_title}"
+  end
+
+  test "philosopher array created" do
+    get '/'
+    assert_not_nil assigns(:philosophers)
+  end
+
+  test "random philosopher selected" do
+    get '/'
+    assert_not_nil assigns(:random_philosopher)
+  end
+  
+  test "random philosopher displayed" do
+    get '/'
+    assert_select("#random_display", "Your random philosopher is #{assigns[:random_philosopher]}.")
   end
 end
